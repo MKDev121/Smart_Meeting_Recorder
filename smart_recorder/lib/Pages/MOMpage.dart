254dc71256094ae38461261e3f66a7fb92d62893
Laksh0511 <kulshresthalakshya03@gmail.com>
2025-05-22 15:06:10 +0530

    Set the background colour to light blye

--- smart_recorder/lib/Pages/MOMpage.dart
+++ smart_recorder/lib/Pages/MOMpage.dart
@@ -5,11 +5,17 @@ class MomPage extends StatelessWidget {
   @override
     Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Color(0xFFB9E6FF),
       appBar: AppBar(
         title:  Text("MoM Page",textAlign: TextAlign.start,),
         centerTitle: true,
         backgroundColor: Color(0xFFB9E6FF),
-        
+      ),
+      body: Container(
+        child: Text('This is the MoM page'),
+        color: Color(0xFF5C95FF),
+        height: 360,
+        width: 720
       ),
     );
   }
--- smart_recorder/lib/main.dart
+++ smart_recorder/lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:smart_recorder/Pages/MOMpage.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: MomPage(), // Set this as the starting screen
+  ));
 }
