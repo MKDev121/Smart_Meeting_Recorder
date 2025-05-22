f387df17601115c1f94d63e98db59755ec7fab06
Laksh0511 <kulshresthalakshya03@gmail.com>
2025-05-22 14:30:50 +0530

    setup the build widget

---
+++ smart_recorder/lib/Pages/MOMpage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MomPage extends StatelessWidget {
+  const MomPage({super.key});
+  @override
+    Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text("MoM Page",textAlign: TextAlign.start,),
+        centerTitle: true,
+        backgroundColor: Color(0xFFB9E6FF),
+        
+      ),
+    );
+  }
+}
+
+
+
+
+
