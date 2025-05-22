b8c38f878da775aa72a6501f4e64f7f819aa5d09
Laksh0511 <kulshresthalakshya03@gmail.com>
2025-05-22 15:37:58 +0530

    Added the heading and made the sizing of the box responsive

--- smart_recorder/lib/Pages/MOMpage.dart
+++ smart_recorder/lib/Pages/MOMpage.dart
@@ -4,18 +4,44 @@ class MomPage extends StatelessWidget {
   const MomPage({super.key});
   @override
     Widget build(BuildContext context){
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: Color(0xFFB9E6FF),
-      appBar: AppBar(
-        title:  Text("MoM Page",textAlign: TextAlign.start,),
-        centerTitle: true,
-        backgroundColor: Color(0xFFB9E6FF),
-      ),
-      body: Container(
-        child: Text('This is the MoM page'),
-        color: Color(0xFF5C95FF),
-        height: 360,
-        width: 720
+      body: Center(
+        child: Container(
+          height: screenHeight * 0.9,
+          width: screenWidth * 0.8,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xFF5C95FF),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            //center and Heading of the page in column
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Minutes of Meeting',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'This is the MoM page',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
