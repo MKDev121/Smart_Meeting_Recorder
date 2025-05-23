d23430ec5fea492f344ba502c18a583dd876ee0a
MKDev121 <meatk121@gmail.com>
2025-05-23 22:09:02 +0530

    Update

--- smart_recorder/lib/main.dart
+++ smart_recorder/lib/main.dart
@@ -1,11 +1,8 @@
+import 'package:smart_recorder/Pages/MOMpage.dart';
 import 'package:flutter/material.dart';
 import 'Pages/homepage.dart';
 import 'Pages/MOMGenerationpage.dart';
-import 'package:smart_recorder/Pages/MOMpage.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
-
 void main() {
-  Gemini.init(apiKey: 'YOUR_API_KEY', enableDebugging: true);
   runApp(const MainApp());
 }
 
