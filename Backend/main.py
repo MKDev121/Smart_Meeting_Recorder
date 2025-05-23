4d38831cf94b11af689bf843c057b10f3cb31842
MKDev121 <meatk121@gmail.com>
2025-05-23 22:06:58 +0530

    Update

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,44 @@
 from openai import OpenAI
+from flask import Flask, request, jsonify
 
 client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
     api_key="AIzaSyA3p2ARwH7nixzzuO5XdMuMqbYVHhbWk4g"
 )
-response = client.chat.completions.create(
+
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "<p>Hello, World!</p>"
+
+@app.route('/getMOM')
+def getMOM():
+    data=request.get_json()
+    mom=generateMOM(data)
+    return jsonify({"mom":mom})
+
+def generateMOM(data):
+    date=data['date']
+    time=data['time']
+    attendees=data['attendees']
+    chairperson=data['chairperson']
+    text=data['text']
+    prompt=f""" Generate minutes of meeting based on following details:
+    Date: {date}
+    Time: {time}
+    Attendees: {attendees}
+    Chairperson: {chairperson}
+    Meeting transcription: {text}
+    Also try to infere agenda and main points by looking at the transcription
+    """
+
+    response = client.chat.completions.create(
     model="gemini-2.5-flash-preview-04-17",
-    messages=[{"role": "user", "content": "Explain quantum computing"}]
-)
-print(response.choices[0].message.content)
+    messages=[{"role": "user", "content":prompt}]
+    )
+    return response.choices[0].message.content   
+    
+
